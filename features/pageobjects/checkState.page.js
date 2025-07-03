ad869f73d7aa136bae8edabefc518aa74cbfb9ae
Valentina Cadena Zapata <82690199+ValentinaCadena@users.noreply.github.com>
2025-07-03 15:39:19 -0500

    Create checkState.page.js

---
+++ features/pageobjects/checkState.page.js
@@ -0,0 +1,18 @@
+import Page from "./page.js";
+
+class CheckStatePage extends Page {
+
+  get numberAccount() {
+    return $('//a[normalize-space()='13344']')
+  }
+
+  async selectAccount(account) {
+    await this.numberAccount().click();
+  }
+
+  open() {
+    return super.open('overview');
+  } 
+}
+
+export default new CheckStatePage();
