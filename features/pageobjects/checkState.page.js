3792bc812232f0ac676e5304e22ba54616972870
Valentina Cadena Zapata <82690199+ValentinaCadena@users.noreply.github.com>
2025-07-09 11:12:04 -0500

    Update checkState.page.js

--- features/pageobjects/checkState.page.js
+++ features/pageobjects/checkState.page.js
@@ -2,35 +2,14 @@ import Page from "./page.js";
 
 class CheckStatePage extends Page {
 
-  get numberSavingsAccount() {
-    return $("//a[normalize-space()='13344']")
-  }
-
   get numberCheckingAccount() {
-    return $("//a[normalize-space()='12345']")
-  }
-
-  get numberLoanAccount() {
-    return $("//a[normalize-space()='13899']")
+    return $("//a[normalize-space()='13011']")
   }
 
   async selectAccount(account) {
-    if (account == 13344) {
-      await expect(this.numberSavingsAccount).toBeExisting();
-      await this.numberSavingsAccount.waitForClickable();
-      await this.numberSavingsAccount.click();
-      
-    } else if (account == 12345) {
       await expect(this.numberCheckingAccount).toBeExisting();
-      await this.numberCheckingAccount.waitForClickable();      
+      await this.numberCheckingAccount.waitForClickable();
       await this.numberCheckingAccount.click();
-      
-    } else {
-      await expect(this.numberLoanAccount).toBeExisting();
-      await this.numberLoanAccount.waitForClickable();
-      await this.numberLoanAccount.click();
-      
-    }
   }
 
   open () {
