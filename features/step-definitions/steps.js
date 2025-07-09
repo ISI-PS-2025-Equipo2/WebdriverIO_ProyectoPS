7e20bfec060788db18e9549bb300b5a2e075343f
Valentina Cadena Zapata <82690199+ValentinaCadena@users.noreply.github.com>
2025-07-09 11:17:16 -0500

    Update steps.js

--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -1,5 +1,4 @@
 import { Given, When, Then } from '@wdio/cucumber-framework';
-import RegisterPage from '../pageobjects/register.page.js';
 import LoginPage from '../pageobjects/login.page.js';
 import CheckStatePage from '../pageobjects/checkState.page.js';
 import TransferPage from '../pageobjects/transfer.page.js';
@@ -7,7 +6,6 @@ import LoanPage from '../pageobjects/loan.page.js';
 
 const pages = {
   login: LoginPage,
-  register: RegisterPage,
   checkState: CheckStatePage,
   transfer: TransferPage,
   requestloan: LoanPage
@@ -33,59 +31,6 @@ Then(/^I should see a text saying (.*)$/, async (message) => {
     await expect($(".title")).toHaveTextContaining(message);
   }
 });
-
-/*//REGISTER
-When(
-  /^I register with (.*) and (.*) and (.*) and (.*) and (.*) and (.*) and (.*) and (.*) and (.*) and (.*) and (.*)$/,
-  async (
-    firstName,
-    lastName,
-    address,
-    city,
-    state,
-    zipCode,
-    phone,
-    ssn,
-    username,
-    password,
-    confirmPassword
-  ) => {
-    await RegisterPage.register(
-      firstName,
-      lastName,
-      address,
-      city,
-      state,
-      zipCode,
-      phone,
-      ssn,
-      username,
-      password,
-      confirmPassword
-    );
-  }
-);
-
-Then(
-  /^I should see a text on the Register result saying (.*)$/,
-  async (message) => {
-    if (message == "Passwords did not match.") {
-      // incorrect password and confirmed password
-      await expect($("span[id='repeatedPassword.errors']")).toBeExisting();
-      await expect(
-        $("span[id='repeatedPassword.errors']")
-      ).toHaveTextContaining(message);
-    } else if (message == "This username already exists.") {
-      //username already exist
-      await expect($(".error")).toBeExisting();
-      await expect($(".error")).toHaveTextContaining(message);
-    } else {
-      // valid data
-      await expect($(".title")).toBeExisting();
-      await expect($(".title")).toHaveTextContaining(message);
-    }
-  }
-);*/
 
 //CHECK ACCOUNT STATE
 When(/^I click on an (.*)$/,
