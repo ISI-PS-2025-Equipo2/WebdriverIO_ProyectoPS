b9ddf19d777a88f443d66b307fe54425698379a6
Valentina Cadena Zapata <82690199+ValentinaCadena@users.noreply.github.com>
2025-07-08 22:20:42 -0500

    Update checkState.page.js

--- features/pageobjects/checkState.page.js
+++ features/pageobjects/checkState.page.js
@@ -1,32 +1,36 @@
 import Page from "./page.js";
 
 class CheckStatePage extends Page {
-  /**
-   * Retorna el array de números de cuenta visibles en la tabla
-   */
-  async getAvailableAccounts() {
-    const accounts = await $$("//table[@id='accountTable']//a");
-    const accountNumbers = await Promise.all(accounts.map(acc => acc.getText()));
-    console.log("📄 Cuentas disponibles actualmente:", accountNumbers);
-    return accountNumbers;
+
+  get numberSavingsAccount() {
+    return $("//a[normalize-space()='13344']")
   }
 
-  /**
-   * Intenta seleccionar la cuenta si está visible en la tabla
-   */
+  get numberCheckingAccount() {
+    return $("//a[normalize-space()='12345']")
+  }
+
+  get numberLoanAccount() {
+    return $("//a[normalize-space()='13899']")
+  }
+
   async selectAccount(account) {
-    const available = await this.getAvailableAccounts();
-    const accountStr = String(account);
-
-    if (!available.includes(accountStr)) {
-      throw new Error(`❌ La cuenta ${accountStr} no está disponible en este momento.`);
+    if (account == 13344) {
+      await expect(numberSavingsAccount).toBeExisting();
+      await this.numberSavingsAccount.waitForClickable();
+      await this.numberSavingsAccount.click();
+      
+    } else if (account == 12345) {
+      await expect(numberCheckingAccount).toBeExisting();
+      await this.numberCheckingAccount.waitForClickable();      
+      await this.numberCheckingAccount.click();
+      
+    } else {
+      await expect(numberLoanAccount).toBeExisting();
+      await this.numberLoanAccount.waitForClickable();
+      await this.numberLoanAccount.click();
+      
     }
-
-    const selector = `//a[normalize-space()='${accountStr}']`;
-    const accountElement = await $(selector);
-
-    await accountElement.waitForClickable({ timeout: 5000 });
-    await accountElement.click();
   }
 
   open () {
